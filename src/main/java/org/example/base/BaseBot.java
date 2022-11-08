18613d777e072fe35b00adb51048c42d6e6b5889
Aliyeva Oydin <mdroid786@gmail.com>
2022-11-08 10:00:21 +0500

    upload

---
+++ src/main/java/org/example/base/BaseBot.java
@@ -0,0 +1,7 @@
+package org.example.base;
+
+public interface BaseBot {
+    String BOT_TOKEN = "5772152044:AAGXDs6VISk1ArZocpLOWZRIKGrvANMOSQ8";
+    String BOT_USERNAME = "https://t.me/order_Foodsbot";
+
+}
